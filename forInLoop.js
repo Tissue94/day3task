8676d72e01ff1d4adb09ab0f89977b0fe3868138
Tissue94 <tissue2979@naver.com>
2021-11-10 19:29:54 +0900

    Initial commit

---
+++ forInLoop.js
@@ -0,0 +1,8 @@
+
+const arr = {a : 1, b : 2, c : 3}
+
+for (let arrKey in arr) {
+    console.log(arr)
+}
+
+//객체 값의 반복
---
+++ forOfLoop.js
@@ -0,0 +1,7 @@
+const arr = [4, 23, 17, 58, 42]
+
+for (let number of arr) {
+    console.log(arr)
+}
+
+//배열 값의 반복
